9d5addaa2c85881d4c575e74e24ffbbfddbffea6
seb <sebastian.thomas@u-dox.com>
2014-01-28 21:28:07 +0000

    Fixes for ie7

--- media/js/script.js
+++ media/js/script.js
@@ -2,7 +2,8 @@
 // For the sake of a quick demo, we're just going to show and hide some elems
 // Easiest way to do that would be to hide them all
 function hideAll() {
-    var boxes = document.getElementsByClassName("content");
+    var boxContainer = document.getElementById("box_container");
+    var boxes = boxContainer.getElementsByTagName("div"); // ok using 'div' as a selector isn't ideal, but: IE7
     for(var i = 0; i < boxes.length; i++){
         boxes[i].style.display = 'none';
     }
