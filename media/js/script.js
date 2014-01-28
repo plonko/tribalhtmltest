71a2731e83ad2ce4ed484dbfefdae9b2532cb62d
seb <sebastian.thomas@u-dox.com>
2014-01-28 20:31:28 +0000

    Fixed script

--- media/js/script.js
+++ media/js/script.js
@@ -1,9 +1,6 @@
-function show(clickerId) {
-    var targetId = document.getElementById("box_"+clickerId);
-    targetId.style.display = 'block';
-    return false;
-}
-
+// Set up our funcs
+// For the sake of a quick demo, we're just going to show and hide some elems
+// Easiest way to do that would be to hide them all
 function hideAll() {
     var boxes = document.getElementsByClassName("content");
     for(var i = 0; i < boxes.length; i++){
@@ -11,14 +8,23 @@ function hideAll() {
     }
 }
 
-var linkContainer = document.getElementById("linkContainer")
+// And show the one you want, using a matching id
+function show(clickerId) {
+    var targetId = document.getElementById("box_"+clickerId);
+    targetId.style.display = 'block';
+}
+
+// Set up your vars
+var linkContainer = document.getElementById("linkContainer");
 var links = linkContainer.getElementsByTagName("a");
 
+// Bind your click handlers
 for(var i = 0; i < links.length; i++){
     var elem = links[i];
     elem.onclick = function(){
         var clickerId = this.id;
         hideAll();
         show(clickerId);
-    };
+        return false;
+    }
 }
